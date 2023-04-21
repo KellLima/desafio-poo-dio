3df5672c85e663a5fd3b24f8086644a2ab04527d
Kell <kennelrak@gmail.com>
2023-04-21 16:12:40 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso();
+
+        curso1.setCargaHoraria(8);
+        curso1.setTitulo("curso Java");
+        curso1.setDescricao("Descrição curso Java");
+
+        Curso curso2 = new Curso();
+        curso2.setCargaHoraria(8);
+        curso2.setTitulo("curso JS");
+        curso2.setDescricao("Descrição curso JS");
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        Mentoria mentoria = new Mentoria();
+
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descrição Mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(mentoria);
+
     }
 }
